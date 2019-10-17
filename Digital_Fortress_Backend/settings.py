d40c7564b57601974bb2a32f84133c325a510422
himanshu272 <himanshu.shekharjha.272@gmail.com>
2019-10-17 12:42:38 +0530

    Add static root

--- Digital_Fortress_Backend/settings.py
+++ Digital_Fortress_Backend/settings.py
@@ -146,6 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
