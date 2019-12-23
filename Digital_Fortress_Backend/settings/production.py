02d69fec5e9bc6708dc6faa4aecfaac976b641ba
himanshu272 <himanshu.shekharjha.272@gmail.com>
2019-12-23 13:06:34 +0530

    Separate settings.py

---
+++ Digital_Fortress_Backend/settings/production.py
@@ -0,0 +1,19 @@
+from .base import *
+from decouple import config, Csv
+
+DEBUG = False
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
