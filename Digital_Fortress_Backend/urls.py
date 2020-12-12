5e5ab36cccb6b73acb482d0d2fa909d335286d80
deadlycoder <ramiz.mollah07@outlook.com>
2020-12-12 23:31:07 +0530

    static update

--- Digital_Fortress_Backend/urls.py
+++ Digital_Fortress_Backend/urls.py
@@ -21,3 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('quiz/', include('Quiz.urls'))
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
