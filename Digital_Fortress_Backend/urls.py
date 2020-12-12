eed0dae4da27e4563d6564f84ab34b4db55560ec
Ramiz mollah <ramiz.mollah07@outlook.com>
2020-12-12 10:15:49 -0800

    Update urls.py

--- Digital_Fortress_Backend/urls.py
+++ Digital_Fortress_Backend/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('quiz/', include('Quiz.urls'))
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
-urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
