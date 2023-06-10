c82e39e5c688a32c5abf495a6c138ccc057c7fa6
Satyam0204 <satyampattanaik007@gmail.com>
2023-06-11 02:31:23 +0530

    resolved merge conflicts

--- Digital_Fortress_Backend/settings.py
+++ Digital_Fortress_Backend/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=False, cast=bool)
 
-#ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 ALLOWED_HOSTS = ["*"]
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 GOOGLE_MAPS_API_KEY = config('GOOGLE_MAPS_API_KEY')
 
